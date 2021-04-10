4463abc55c002061dd5d6f6da2bbd086ee1645b3
AshwinAashu <80351416+AshwinAashu@users.noreply.github.com>
2021-04-10 11:51:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import DragDrop from './component/dragdrop';
+
+function App() {
+
+  return (
+    <div className="App">
+      <DragDrop/>
+    </div>
+  );
+}
+
+export default App;
